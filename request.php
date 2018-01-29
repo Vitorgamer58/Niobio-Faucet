6525744f0565d5340927cc24d80e1fd7468939a5
Vitorgamer58 <vitorgamer58@outlook.com>
2018-01-29 18:59:41 +0000

    Update request.php

--- request.php
+++ request.php
@@ -51,7 +51,7 @@ if ($recaptcha->set()) {
             exit();
         }
 
-        $bitcoin = new jsonRPCClient('http://127.0.0.1:8070/json_rpc');
+        $bitcoin = new jsonRPCClient('http://127.0.0.1:8317/json_rpc');
         $balance = $bitcoin->getbalance();
         $balanceDisponible = $balance['available_balance'];
         $transactionFee = 10000;
